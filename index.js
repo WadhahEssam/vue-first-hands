2dc0191ac801a22a116a86e29ca087cead221d45
Wadah Esam <wadah-wleed@hotmail.com>
2018-10-13 16:07:16 +0300

    completing the first seciton of documentation

--- index.js
+++ index.js
@@ -34,3 +34,41 @@ var app4 = new Vue({
     ]
   }
 })
+
+// 5
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message.split('').reverse().join('');
+    }
+  }
+})
+
+// 6
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+// 7 vue components
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
